338be75943f307bce64b953b0da2782933fc4646
Juris Pikuls <juris.pikuls@forge.com>
2022-05-02 14:19:52 +0300

    1x commits failu pievienosanai

---
+++ world7.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+//#include<conio.h>
+//#include<stdio.h>
+
+int main ()
+
+//hello world izvade
+{
+    string x, y;
+cout<<"Ieraksti savu Vardu: \n";
+cin>>x;
+cout<<"Ieraksti savu Uzvardu: \n";
+cin>>y;
+
+cout<<"Tu ierakstiji: "<<x<<" "<<y<<" !!!";
+
+//return 0;
+}
